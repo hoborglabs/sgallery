03ed7a0f5510d6c34df4d86a45d404fd008ef975
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2013-01-16 17:10:11 +0000

    [refs #23] Adding almond to JS app build

--- scripts/hoborglabs/app.build.js
+++ scripts/hoborglabs/app.build.js
@@ -1,6 +1,7 @@
 ({
     baseUrl: "./..",
-    name: "hoborglabs/app",
+    name: "almond",
+    include: ["hoborglabs/app"],
     out: "../../dist/static/scripts/hoborglabs/app.js",
     preserveLicenseComments: false,
 })
